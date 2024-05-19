27123dfc99c3572393ea713bc39ca920b6d4be2d
Mujtaba <126389386+Mujtaba500@users.noreply.github.com>
2024-05-19 14:09:19 +0500

    crawl page  recursive working

--- crawl.js
+++ crawl.js
@@ -1,6 +1,20 @@
 import { JSDOM } from "jsdom";
 
-async function crawlPage(currentURL) {
+async function crawlPage(baseURL, currentURL, pages) {
+  const baseURLObj = new URL(baseURL);
+  const currentURLObj = new URL(currentURL);
+
+  if (baseURLObj.hostname !== currentURLObj.hostname) {
+    return pages;
+  }
+
+  const normalizedCurrentURL = normalizeURL(currentURL);
+  if (pages[normalizedCurrentURL] > 0) {
+    pages[normalizedCurrentURL]++;
+    return pages;
+  }
+  pages[normalizedCurrentURL] = 1;
+
   console.log(`Actively crawling ${currentURL}`);
 
   try {
@@ -9,7 +23,7 @@ async function crawlPage(currentURL) {
       console.log(
         `error with status code: ${res.status} on page: ${currentURL}`
       );
-      return;
+      return pages;
     }
 
     const contentType = res.headers.get("content-type");
@@ -17,12 +31,20 @@ async function crawlPage(currentURL) {
       console.log(
         `non html response, content type : ${contentType}, on page: ${currentURL} `
       );
-      return;
+      return pages;
     }
-    console.log(await res.text());
+
+    const htmlBody = await res.text();
+
+    const nextURLs = getURLSfromHTML(htmlBody, baseURL);
+
+    for (const nextURL of nextURLs) {
+      pages = await crawlPage(baseURL, nextURL, pages);
+    }
   } catch (err) {
     console.log(`error in fetch: ${err.message}, on page: ${currentURL}`);
   }
+  return pages;
 }
 
 function getURLSfromHTML(htmlBody, baseURL) {
